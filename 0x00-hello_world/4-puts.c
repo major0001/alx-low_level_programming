022f9328b4e1e45996d7c9fbc6202abce45d8310
major0001 <alexmutune746@gmail.com>
2022-06-03 05:17:09 +0000

    C programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,6 @@
  * main - Write '"Programming is like building a multilingual puzzle'
  *Return: Always 0 (success)
 */
-
 int main(void)
 {
 puts("\"Programming is like buliding a multilingual puzzle");
