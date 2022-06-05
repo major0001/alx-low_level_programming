39a82e8d8857e2a8fa0b5a6b8410e34e29dfa895
major0001 <alexmutune746@gmail.com>
2022-06-05 15:19:25 -0700

    Everything nice

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,6 +10,5 @@ int main(void)
 	char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(2, ch, sizeof(ch));
-	fprintf(stderr, "This is message 2\n");
 	return (1);
 }
