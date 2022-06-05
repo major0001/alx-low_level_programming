dd31b22636a0927c855b259dadad6a41264fa503
major0001 <alexmutune746@gmail.com>
2022-06-05 16:27:27 -0700

    Doing it allover again

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,9 +5,8 @@
  * Return:Always 1 (Success)
  */
 int main(void)
-
 {
-char ch []= ("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);	
+char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 write(2, ch, sizeof(ch));
 return (1);
