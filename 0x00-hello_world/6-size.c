28e1cb01994b613706968c3a2b47344336a91589
major0001 <alexmutune746@gmail.com>
2022-06-05 18:12:25 -0700

    The originals

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,7 +16,7 @@ int main(void)
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(aint));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(alg));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(allg));
-	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(aflt));
+	printf("Size of a float: %lu byte(s)", (unsigned long)sizeof(aflt));
 
 	return (0);
 }
