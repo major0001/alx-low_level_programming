3a23fc0fdb79876d62dcb7523010cc57c0d1e9cb
major0001 <alexmutune746@gmail.com>
2022-06-05 17:15:35 -0700

    Woow

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,10 @@
  * main - Prints out a string to stdout
  *
  * Description: this prints out string
- * 
  * Return: Always 0 if success
 */
 int main(void)
 {
-      	
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
