0036391b2b8ee31da2e06b923c52034ca86711c1
major0001 <alexmutune746@gmail.com>
2022-06-07 21:00:09 -0700

    It is a great experience in the C world

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ * _islower - returns 1 if a is lowercase, 0 otherwise
+ * @a: integer to be tested
+ * Return: 1 or 0
+ */
+int _islower(int a)
+{
+	return (a >= 97 && a <= 122);
+}
