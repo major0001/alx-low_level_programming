cf968b15bfcc3ee2b36aa4b12f5d8594bef082be
major0001 <alexmutune746@gmail.com>
2022-06-13 20:53:26 -0700

    Major

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - swap the values of variables
+ * @a: pointer1
+ * @b: pointer2
+ * Return: void
+*/
+
+void swap_int(int *a, int *b)
+{
+int s = *a;
+*a = *b;
+*b = s;
+}
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - find the length of a string
+ * @s: pointer to the string to check
+ * Return: length of the string passed to it
+*/
+
+
+int _strlen(char *s)
+{
+int i = 0;
+while (s[i])
+	i++;
+
+return (i);
+}
